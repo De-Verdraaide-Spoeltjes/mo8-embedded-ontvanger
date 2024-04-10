077857791d154dfe3fc56da432e1a37afd9408a6
Tommy de Wever <tommy.de.wever@gmail.com>
2024-04-10 12:46:12 +0200

    Add status led and button

--- src/main.c
+++ src/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include "platform.h"
 #include "xil_printf.h"
+#include "xtime_l.h"
+#include "xil_printf.h"
+#include "xgpio.h"
+#include <stdbool.h>
 
 #include "defines.h"
 #include "generate_rsa_keys.h"
@@ -8,12 +12,25 @@
 #include "rsa_decryption.h"
 #include "display.h"
 
+#define STATUS_BLINK 200 //ms
+#define STATUS_LED_DEVICE_ID    XPAR_CONNECTION_EMBEDDED_STATUS_LED_DEVICE_ID
+#define BTNS_DEVICE_ID          XPAR_CONNECTION_EMBEDDED_BUTTONS_DEVICE_ID
+
+
+
 rsaData RSAData;
+XGpio leds, buttons;
+
+void statusLED();
 
 int main()
 {
     init_platform();
     print("Starting embedded application\n\r");
+    XGpio_Initialize(&leds, STATUS_LED_DEVICE_ID);
+    XGpio_Initialize(&buttons, BTNS_DEVICE_ID);
+    XGpio_SetDataDirection(&leds, 1, 0x0);
+    XGpio_SetDataDirection(&buttons, 1, 0x1);
 
     XStatus status;
 
@@ -48,9 +65,35 @@ int main()
     print("Embedded application initialized\n\r");
 
     while(1) {
+        statusLED();
         runKeyTransmitter(&RSAData);
+
+        static bool prevButtonState = false;
+
+        bool buttonState = XGpio_DiscreteRead(&buttons, 1) & 0x1;
+
+        if (buttonState && !prevButtonState && buttonState == true) {
+            xil_printf("Button pressed\n\r");
+            initDisplay();
+        }
+
+        prevButtonState = buttonState;
     }
 
     cleanup_platform();
     return 0;
+}
+
+
+void statusLED() {
+    XTime tNow;
+    static XTime tOld;
+	static bool state;
+    XTime_GetTime(&tNow);
+    if (tOld + NS_TO_TIME(STATUS_BLINK) * 1000 < tNow) {
+        // Toggle status LED
+        XGpio_DiscreteWrite(&leds, 1, state << 1);
+        state = !state;
+        tOld = tNow;
+    }
 }
